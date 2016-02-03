ec300538b6eb960c152a332ae319d7f16987f1b9
Donghyun <donghyun208@gmail.com>
2016-02-02 19:38:01 -0800

    add google analytics, add mathjax CDN

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,9 @@ module.exports = function (grunt) {
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+
 
   // Automatically load required Grunt tasks
   require('jit-grunt')(grunt, {
@@ -220,7 +223,7 @@ module.exports = function (grunt) {
             }
           }
       }
-    }, 
+    },
 
     // Renames files for browser caching purposes
     filerev: {
@@ -423,6 +426,12 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
     }
   });
 
