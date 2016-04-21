983175485f4f38f6b00e3e6e26cf053c4faf9a9c
Donghyun <donghyun208@gmail.com>
2016-04-20 19:12:45 -0700

    move data and some data-specific logic from moPlot directive to Huckel controller. add nodal planes

--- app/scripts/controllers/huckel.js
+++ app/scripts/controllers/huckel.js
@@ -14,4 +14,47 @@ angular.module('qmWaveApp')
       floor: 0,
       ceil: 360
     };
+
+    var psi1 = {energy: -2, degen:  0, vec: [ 0.40824829,  0.40824829,  0.40824829,  0.40824829,  0.40824829,  0.40824829], nodes: []};
+    var psi2 = {energy: -1, degen: -1, vec: [-0.57735027, -0.28867513,  0.28867513,  0.57735027,  0.28867513, -0.28867513], nodes: [0]};
+    var psi3 = {energy: -1, degen:  1, vec: [ 0.0,         0.5,         0.5,         0.0,        -0.5,        -0.5       ], nodes: [90]};
+    var psi4 = {energy:  1, degen: -1, vec: [ 0.57735027, -0.28867513, -0.28867513,  0.57735027, -0.28867513, -0.28867513], nodes: [45, 135]};
+    var psi5 = {energy:  1, degen:  1, vec: [ 0.0,        -0.5,         0.5,         0.0,        -0.5,         0.5       ], nodes: [0, 90]};
+    var psi6 = {energy:  2, degen:  0, vec: [ 0.40824829, -0.40824829,  0.40824829, -0.40824829,  0.40824829, -0.40824829], nodes: [0, 60, 120]};
+
+    var psiB_p = angular.copy(psi2)
+    var psiB_m = angular.copy(psi3)
+    var psiA_p = angular.copy(psi4)
+    var psiA_m = angular.copy(psi5)
+
+    $scope.benzene = [psi1, psi2, psi3, psi4, psi5, psi6];
+    $scope.benzene_rotate = [psi1, psiB_p, psiB_m, psiA_p, psiA_m, psi6]
+
+    // var self = $scope.
+    $scope.$watch(function() {
+      return $scope.mix_angle
+    }, function(nv) {
+      mix(psi2, psi3, psiB_p, psiB_m, $scope.mix_angle)
+      mix(psi4, psi5, psiA_p, psiA_m, $scope.mix_angle)
+      psiB_p.nodes = psi2.nodes.map(function(x) { return x + $scope.mix_angle })
+      psiB_m.nodes = psi3.nodes.map(function(x) { return x + $scope.mix_angle })
+      psiA_p.nodes = psi4.nodes.map(function(x) { return x + $scope.mix_angle / 2.0})
+      psiA_m.nodes = psi5.nodes.map(function(x) { return x + $scope.mix_angle / 2.0})
+    });
+
+    function mix(phi_1, phi_2, phi_A, phi_B, mix_angle) {
+      var mat = rot_mat(mix_angle);
+      for(var i = 0; i < phi_1.vec.length; i++){
+        phi_A.vec[i] = phi_1.vec[i] * mat[0][0] + phi_2.vec[i] * mat[1][0];
+        phi_B.vec[i] = phi_1.vec[i] * mat[0][1] + phi_2.vec[i] * mat[1][1];
+      }
+    };
+
+    function rot_mat(angle) {
+      angle = angle / 360.0 * 2 * Math.PI
+      return [[Math.cos(angle), -Math.sin(angle)], [Math.sin(angle), Math.cos(angle)]]
+    };
+
   }]);
+
+
--- app/scripts/directives/moplot.js
+++ app/scripts/directives/moplot.js
@@ -7,12 +7,15 @@
  * # moplot
  */
 angular.module('qmWaveApp')
-  .directive('moPlot', ['d3', function(d3Service) {
+  // .directive('moPlot', ['d3', function(d3Service) {
+  .directive('moPlot', [function() {
     return {
       restrict: 'EA',
-      scope: {angle: '='},
+      scope: {eig: '=',
+              angle: '='},
       link: function(scope, element, attrs) {
-        d3Service.d3().then(function(d3) {
+        // d3Service.d3().then(function(d3) {
+        if (1) {
           var margin = parseInt(attrs.margin) || 20,
               plotHeight = parseInt(attrs.plotHeight) || 20,
               plotWidth = parseInt(attrs.plotWidth) || 20;
@@ -28,14 +31,6 @@ angular.module('qmWaveApp')
           // };
 
           // hard-code data
-          scope.data = [
-            {energy: -2, degen:  0, vec: [ 0.40824829,  0.40824829,  0.40824829,  0.40824829,  0.40824829,  0.40824829]},
-            {energy: -1, degen: -1, vec: [-0.57735027, -0.28867513,  0.28867513,  0.57735027,  0.28867513, -0.28867513]},
-            {energy: -1, degen:  1, vec: [ 0.0,         0.5,         0.5,         0.0,        -0.5,        -0.5       ]},
-            {energy:  1, degen: -1, vec: [ 0.57735027, -0.28867513, -0.28867513,  0.57735027, -0.28867513, -0.28867513]},
-            {energy:  1, degen:  1, vec: [ 0.0,        -0.5,         0.5,         0.0,        -0.5,         0.5       ]},
-            {energy:  2, degen:  0, vec: [ 0.40824829, -0.40824829,  0.40824829, -0.40824829,  0.40824829, -0.40824829]}
-          ];
 
           // returns the proper x,y coordinate for all circles:
           // function pos(data)
@@ -48,7 +43,7 @@ angular.module('qmWaveApp')
             return self.angle
           }, function(nv) {
             if (!firsttime) {
-              scope.rerender(nv);
+              scope.rerender(self.eig, nv);
             }
             firsttime = 0
           });
@@ -69,170 +64,154 @@ angular.module('qmWaveApp')
           var hex_radius = 20;
 
           var degeneracy_offset = 60;
-          var a0_radius = 20;
+          var a0_radius = 18;
 
           var molecular_orbital;
-          var mo_group;
           var color = d3.scale.category20();
 
-          scope.rerender = function(angle) {
-            var mat = rot_mat(angle)
-            mo_group.each(function(mo) {
-              if (mo.degen !== 0) {
-                if (mo.energy == -1) {
-                  var phi1 = [-0.57735027, -0.28867513,  0.28867513,  0.57735027,  0.28867513, -0.28867513]
-                  var phi2 = [ 0.0,         0.5,         0.5,         0.0,        -0.5,        -0.5       ]
+          var energyScale = d3.scale.linear()
+                            .domain([2,-2])
+                            .range([50, 250]);
+          var axisLabel = {"0": "\u03B1",
+                         "-1": "\u03B1 + \u03B2",
+                         "-2": "\u03B1 + 2\u03B2",
+                         "1": "\u03B1 - \u03B2",
+                         "2": "\u03B1 - 2\u03B2" };
+
+          var subscripts = '₁₂₃₄₅₆';
+
+          scope.rerender = function(data, mix_angle) {
+
+            svg.selectAll('g.moGroup')
+              .each(function(mo, index) {
+                if (mo.degen !== 0) {
+                  var molecular_orbital = d3.select(this)
+                  molecular_orbital
+                  .selectAll('circle')
+                  .data(data[index].vec)
+                  .attr('r', function(coef) { return Math.abs(coef) * a0_radius}) // use abs instead of pow so that the circle area corresponds to magnitude instead of circle radius
+                  .attr('fill', function(coef) { return color(Math.sign(coef)); })
+
+                  molecular_orbital
+                  .selectAll('line.nodalPlane')
+                  .data(data[index].nodes)
+                  .attr('x1', function(angle, index) {return  angle_to_line_segment(angle)[0] * (hex_radius * 1.3)})
+                  .attr('y1', function(angle, index) {return  angle_to_line_segment(angle)[1] * (hex_radius * 1.3)})
+                  .attr('x2', function(angle, index) {return -angle_to_line_segment(angle)[0] * (hex_radius * 1.3)})
+                  .attr('y2', function(angle, index) {return -angle_to_line_segment(angle)[1] * (hex_radius * 1.3)})
                 }
-                else {
-                  var phi1 = [ 0.57735027, -0.28867513, -0.28867513,  0.57735027, -0.28867513, -0.28867513]
-                  var phi2 = [ 0.0,        -0.5,         0.5,         0.0,        -0.5,         0.5       ]
-                }
-                if (mo.degen === -1) {
-                  var psi = []
-                  phi1 = phi1.map(function(x) {return x * mat[0][0]})
-                  phi2 = phi2.map(function(x) {return x * mat[1][0]})
-                  for(var i = 0; i < phi1.length; i++){
-                    psi.push(phi1[i] + phi2[i]);
-                  }
-                }
-                else {
-                  var psi = []
-                  phi1 = phi1.map(function(x) {return x * mat[0][1]})
-                  phi2 = phi2.map(function(x) {return x * mat[1][1]})
-                  for(var i = 0; i < phi1.length; i++){
-                    psi.push(phi1[i] + phi2[i]);
-                  }
-                }
-                molecular_orbital = d3.select(this)
-                .selectAll('circle')
-                .attr('r', function(coef, index) { return Math.abs(psi[index]) * a0_radius}) // use abs instead of pow so that the circle area corresponds to magnitude instead of circle radius
-                .attr('fill', function(coef, index) { return color(Math.sign(psi[index])); })
-              }
-            })
+              });
           }
 
           scope.render = function(data) {
-            // remove all previous items before render
-            svg.selectAll('*').remove();
-
-            // If we don't pass any data, return out of the element
-            if (!data) return;
-
-            // setup variables
-            // var width = d3.select(element[0]).node().offsetWidth - margin,
-            //     // calculate the height
-            //     height = scope.data.length * (barHeight + barPadding),
-            //     // Use the category20() scale function for multicolor support
-            //     // our xScale
-            //     xScale = d3.scale.linear()
-            //       .domain([0, d3.max(data, function(d) {
-            //         return d.score;
-            //       })])
-            //       .range([0, width]);
-
-            // set the height based on the calculations above
-            var energyScale = d3.scale.linear()
-                              .domain([2,-2])
-                              .range([50, 250]);
-            var dataset = {"0": "\u03B1",
-                           "-1": "\u03B1 + \u03B2",
-                           "-2": "\u03B1 + 2\u03B2",
-                           "1": "\u03B1 - \u03B2",
-                           "2": "\u03B1 - 2\u03B2" }
             var yAxis = d3.svg.axis()
                   .scale(energyScale)
                   .orient("left")
                   .ticks(5)
                   .tickFormat(function(d) {
-                    return dataset[d];
+                    return axisLabel[d];
                   })
+            svg.append("g")
+              .attr("class", "axis")
+              .attr("transform", "translate(" + 50 + ",0)")
+              .call(yAxis)
 
-
-
-            mo_group = svg.selectAll('g')
+            svg.append("g")
+              .selectAll('g')
               .data(data).enter()
               .append('g')
+              .attr('class', 'moGroup')
               .each(function(mo, index) {
-                molecular_orbital = d3.select(this)
+                molecular_orbital = d3.select(this);
+
+                // NODE lines
+                molecular_orbital
+                .selectAll('line')
+                .data(mo.nodes).enter()
+                .append('line')
+                .attr('class', 'nodalPlane')
+                .attr('x1', function(angle, index) {return  angle_to_line_segment(angle)[0] * (hex_radius * 1.3)})
+                .attr('y1', function(angle, index) {return  angle_to_line_segment(angle)[1] * (hex_radius * 1.3)})
+                .attr('x2', function(angle, index) {return -angle_to_line_segment(angle)[0] * (hex_radius * 1.3)})
+                .attr('y2', function(angle, index) {return -angle_to_line_segment(angle)[1] * (hex_radius * 1.3)})
+                .attr('stroke', "red")
+                .style("stroke-dasharray", ("3, 3"))
+                .style("stroke-opacity", 0.7)
+                .attr('stroke-width', 0)
+                .transition()
+                .duration(2000)
+                .attr('stroke-width', 1.5)
+
+                // LOBE circles
+                molecular_orbital
                 .selectAll('circle')
                 .data(mo.vec).enter()
                 .append('circle')
                 .attr('cx', function(coef, index) {
-                  var x = hex_coords[index][0] * hex_radius + 160;
-                  if (mo.degen !== 0) {
-                    x += mo.degen * degeneracy_offset;
-                  }
-                  return x
+                  return hex_coords[index][0] * hex_radius;
                 })
                 .attr('cy', function(coef, index) {
-                  return hex_coords[index][1] * hex_radius + energyScale(mo.energy);
+                  return hex_coords[index][1] * hex_radius;
                 })
                 .attr('fill', function(coef) { return color(Math.sign(coef)); })
                 .attr('r', 0)
                 .transition()
-                .duration(1000)
+                .duration(2000)
                 .attr('r', function(coef) { return Math.abs(coef) * a0_radius}) // use abs instead of pow so that the circle area corresponds to magnitude instead of circle radius
-                molecular_orbital
 
-                var subscripts = '₁₂₃₄₅₆'
-                var text = svg.append("foreignObject")
+                molecular_orbital.append('text')
                 .attr("width",100)
                 .attr("height",100)
                 // text.text("$$\\psi_" + (index + 1)+ "$$")
-                text.text("\u03C8" + subscripts[index])
-                .attr('x', function(coef, index) {
-                  var x = 185;
-                  if (mo.degen !== 0) {
-                    x += mo.degen * degeneracy_offset;
-                  }
-                  return x
-                })
-                .attr('y', energyScale(mo.energy) - 12)
-              })
-            svg.selectAll('g')
-              .append('g')
-              .each(function(mo, index) {
-                var molecular_orbital = d3.select(this)
+                .text("\u03C8" + subscripts[index])
+                .attr('x', '25')
+                .attr('y', '-12')
+
+
+                // HEX lines
+                molecular_orbital
+                .append('g')
                 .selectAll('line')
-                .data(mo.vec).enter()
+                .data(hex_coords).enter()
                 .append('line')
-                .attr('x1', function(coef, index) {
-                  var x = hex_coords[index][0] * hex_radius + 160;
-                  if (mo.degen !== 0) {
-                    x += mo.degen * degeneracy_offset;
-                  }
-                  return x
+                .attr('x1', function(point, index) {
+                  return point[0] * hex_radius;
                 })
-                .attr('y1', function(coef, index) {
-                  return hex_coords[index][1] * hex_radius + energyScale(mo.energy);
+                .attr('y1', function(point, index) {
+                  return point[1] * hex_radius;
                 })
-                .attr('x2', function(coef, index) {
-                  var x = hex_coords[(index + 1) % 6][0] * hex_radius + 160;
-                  if (mo.degen !== 0) {
-                    x += mo.degen * degeneracy_offset;
-                  }
-                  return x
+                .attr('x2', function(point, index) {
+                  return hex_coords[(index + 1) % 6][0] * hex_radius;
                 })
-                .attr('y2', function(coef, index) {
-                  return hex_coords[(index + 1) % 6][1] * hex_radius + energyScale(mo.energy);
+                .attr('y2', function(point, index) {
+                  return hex_coords[(index + 1) % 6][1] * hex_radius;
                 })
                 .attr('stroke', "gray")
                 .attr('stroke-width', 0)
                 .transition()
-                .duration(1000)
+                .duration(2000)
                 .attr('stroke-width', 1)
               })
-              .append("g")
-              .attr("class", "axis")
-              .attr("transform", "translate(" + 50 + ",0)")
-              .call(yAxis)
+              .attr("transform", function(mo, index) {
+                  var x = 160;
+                  if (mo.degen !== 0) {
+                    x += mo.degen * degeneracy_offset;
+                  }
+                  var y = energyScale(mo.energy);
+                return "translate(" + x + ',' + y + ")"; })
           };
 
-          scope.render(scope.data)
-        });
+          scope.render(scope.eig)
+        };
       }
     }
 }]);
+
+function angle_to_line_segment(angle) {
+  var x = Math.cos(angle / 360.0 * 2 * Math.PI)
+  var y = Math.sin(angle / 360.0 * 2 * Math.PI)
+  return [x, y]
+}
 
 function rot_mat(angle) {
   angle = angle / 360.0 * 2 * Math.PI
